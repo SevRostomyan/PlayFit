06d9bc99ed46ab066f84da952fe67ff139a9720f
SevRostomyan <41015978+SevRostomyan@users.noreply.github.com>
2023-12-15 00:22:23 +0100

    dev4-PLAYF-4: - Skapade entitet med grundläggande attribut för en medlem Member.java: - Har även skapat en enum för roller och kopplat till entiteten Member

---
+++ members/src/main/java/playfit/se/members/entity/Member.java
@@ -0,0 +1,28 @@
+package playfit.se.members.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import playfit.se.members.enums.UserRole;
+
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String adress;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    //private List<Notifications> notifications;
+}
---
+++ members/src/main/java/playfit/se/members/enums/UserRole.java
@@ -0,0 +1,8 @@
+package playfit.se.members.enums;
+
+public enum UserRole {
+    MEDLEM,
+    ADMIN,
+    ORG_KONTAKTPERSON,
+    Tränare
+}
