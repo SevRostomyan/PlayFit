18f37b9b2d0d908bb397f749bbdfa32e9511af44
ramyl <ramylayous@hotmail.com>
2024-01-11 14:34:22 +0100

    changed the names to be kamel case.

--- members/src/main/java/playfit/se/members/entity/OrganizationClubEntity.java
+++ members/src/main/java/playfit/se/members/entity/OrganizationClubEntity.java
@@ -5,23 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Data
-@Table
-@NoArgsConstructor
-@AllArgsConstructor
-public class OrganizationClubEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "OrganizationClubGenerator")
-    @SequenceGenerator(name = "OrganizationClubGenerator", sequenceName = "OrganizationClubSeq", allocationSize = 1)
-    private Long id;
-    private String orgNr;
-    @OneToOne
-    private UserEntity representative;
-    private String address;
-    private String zipCode;
-    private String city;
-    private String mobile;
-    private String eMail;
-    private String password;
+    @Entity
+    @Data
+    @Table
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class OrganizationClubEntity {
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "OrganizationClubGenerator")
+        @SequenceGenerator(name = "OrganizationClubGenerator", sequenceName = "OrganizationClubSeq", allocationSize = 1)
+        private Long id;
+        private String orgNr;
+        @OneToOne
+        private UserEntity representative;
+        private String address;
+        private String zipCode;
+        private String city;
+        private String mobile;
+        private String eMail;
+        private String password;
 }
