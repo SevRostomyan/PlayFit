eb21379e35d44ee392eee12218ea6d47ad5183ff
ramyl <ramylayous@hotmail.com>
2024-02-04 15:30:44 +0100

    I implemented PasswordConfig class in the package config, that defines a PasswordEncoder bean using BCrypt hashing providing secure password encoding and decoding functionality. Implemented SecurityConfiguration class with methods securityFilterChain to defines rules for permitting or restricting access to different endpoints, and authenticationProvider to configure the mechanism to authenticate users based on their credentials

---
+++ members/src/main/java/playfit/se/members/config/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package playfit.se.members.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import playfit.se.members.services.UserService;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+   private final JwtAuthenticationFilter jwtAuthenticationFilter;
+   private final UserService userService;
+   private final PasswordEncoder passwordEncoder;
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(authorize-> authorize
+                       .requestMatchers(HttpMethod.POST, "/api/v1/**")
+                       .permitAll()
+                       .anyRequest()
+                       .authenticated()
+               )
+               .authenticationProvider(authenticationProvider())
+               .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+       return http.build();
+   }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService.userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+}
