145bd41a817465fbeb0ff8b50517b85205e61042
Liver Nissan <storegame352@gmail.com>
2024-02-16 10:40:46 +0100

    update information about club

---
+++ members/src/main/java/playfit/se/members/DTOs/InfoClubDTO.java
@@ -0,0 +1,14 @@
+package playfit.se.members.DTOs;
+
+import lombok.Data;
+
+
+@Data
+public class InfoClubDTO {
+    private String orgName;
+    private String address;
+    private String zipCode;
+    private String city;
+    private String mobile;
+
+}
