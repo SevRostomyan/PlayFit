3aba03da403f86132e4585888c148670a6cefd3a
SevRostomyan <41015978+SevRostomyan@users.noreply.github.com>
2024-03-02 22:52:41 +0100

    - Implementerat ny funktionalitet för att skapa pris- och prenumerationsobjekt. - Utvecklat API-endpoints för att hämta listor över aktuella priser och prenumerationer. Dessa endpoints tillhandahåller ID:n som kan användas i frontenden för att skicka förfrågningar till andra endpoints, vilket underlättar skapandet av sessioner, aktiviteter, användare och fakturor. - Inkluderat de nya endpointsen i SecurityConfiguration för förbättrad säkerhet och åtkomsthantering.

--- members/src/main/java/playfit/se/members/repositories/AttendanceRepository.java
+++ members/src/main/java/playfit/se/members/repositories/AttendanceRepository.java
@@ -3,7 +3,6 @@ package playfit.se.members.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import playfit.se.members.entities.Attendance;
-import playfit.se.members.entities.SessionEntity;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
